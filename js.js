c09b0366a550398f4304498cf2dcb20f4b835b58
hirsty777 <777hirsty@gmail.com>
2022-03-30 16:04:34 +0400

    fr

--- js.js
+++ js.js
@@ -1,72 +1,246 @@
-
-
-
 var jinputValue=document.getElementById('inputValue');
 const jaudio=document.getElementById('audio');
 const jenterBTN=document.getElementById('enterBTN');
+const jtopText=document.getElementById("topText");
+
+jaudio.innerHTML='';
+
+const juli=document.querySelector('.uli');
+var ishidden=true;
+var x=0;
+
+// window.onload=fetchTopSongsOfDay();
+
+// async function fetchTopSongsOfDay(){
+//     await fetch(`https://api.napster.com/v2.2/tracks/top?range=day`,{
+        
+//         method:'GET',
+//         headers:{
+//             'apikey':'N2EwNWJiZWUtOGVkMi00YmJjLTgwOWMtY2FiMWQ3MDI1ZjYx'
+//         }
+//     }).then(res=>res.json())
+//     .then(function aa(res){
+
+//         for(var i=0;i<res.tracks.length;i++){
+//         jaudio.innerHTML+=`
+//         <div class="box">
+//         <img src="https://api.napster.com/imageserver/v2/artists/${res.tracks[i].artistId}/images/356x237.jpg">
+//         <span class="subtitle">${res.tracks[i].artistName}</span>
+//         <span class="title">${res.tracks[i].name}</span>
+//         <audio src="${res.tracks[i].previewURL}"controls></audio>   
+//         </div>
+//         `
+//         }
+//         console.log(res);
+//     });
+// };
 
 
 
 
 
-jenterBTN.addEventListener('click',function(){
-     console.log(jinputValue.value) ;
-    fetch(`https://shazam.p.rapidapi.com/search?term=${jinputValue.value}&locale=en-US&offset=0&limit=5`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "shazam.p.rapidapi.com",
-		"x-rapidapi-key": "eb3cff4953msh6bf25d5812fb257p186050jsn0d65cd9976fa"
-	}
-})
-.then(response =>response.json()
-     
-).then(function fff(data){
-    console.log(data.tracks.hits[0].track.images.background);
-jaudio.innerHTML=
-`<div class="box">
-<img src="${data.tracks.hits[0].track.images.background}">
-<span class="subtitle">${data.tracks.hits[0].track.subtitle}</span>
-<span class="title">${data.tracks.hits[0].track.title}</span>
-<audio src="${data.tracks.hits[0].track.hub.actions[1].uri}"controls></audio>
-</div>
-`
-+
-`<div class="box">
-<img src="${data.tracks.hits[1].track.images.background}">
-<span class="subtitle">${data.tracks.hits[1].track.subtitle}</span>
-<span class="title">${data.tracks.hits[1].track.title}</span>
-<audio src="${data.tracks.hits[1].track.hub.actions[1].uri}"controls></audio>
-</div>
-`
-+
-`<div class="box">
-<img src="${data.tracks.hits[2].track.images.background}">
-<span class="subtitle">${data.tracks.hits[2].track.subtitle}</span>
-<span class="title">${data.tracks.hits[2].track.title}</span>
-<audio src="${data.tracks.hits[2].track.hub.actions[1].uri}"controls></audio>
-</div>
-`
-+
-`<div class="box">
-<img src="${data.tracks.hits[3].track.images.background}">
-<span class="subtitle">${data.tracks.hits[3].track.subtitle}</span>
-<span class="title">${data.tracks.hits[3].track.title}</span>
-<audio src="${data.tracks.hits[3].track.hub.actions[1].uri}"controls></audio>
-</div>
-`
+
+
+//togle hamburger menu=========================
+const jnavBAr=document.querySelector('.navBar');
+const jcheck=document.getElementById('check');
+jcheck.addEventListener('click',function(){
+       jnavBAr.classList.toggle('show');
+       
 });
 
 
+function  TopTracks(){
+    if(ishidden){
+        juli.classList.remove("hiden");
+        ishidden=false;
+    }else{
+        juli.classList.add("hiden");
+        ishidden=true;
+    };
+};   
+    
+   
+    // jaudio.innerHTML='';
+    // await fetch(`https://api.napster.com/v2.2/tracks/top?range=week`,{
+        
+    //     method:'GET',
+    //     headers:{
+    //         'apikey':'N2EwNWJiZWUtOGVkMi00YmJjLTgwOWMtY2FiMWQ3MDI1ZjYx'
+    //     }
+    // }).then(res=>res.json())
+    // .then(function aa(res){
 
+    //     for(var i=0;i<res.tracks.length;i++){
+    //     jaudio.innerHTML+=`
+    //     <div class="box">
+    //     <img src="https://api.napster.com/imageserver/v2/artists/${res.tracks[i].artistId}/images/356x237.jpg">
+    //     <span class="subtitle">${res.tracks[i].artistName}</span>
+    //     <span class="title">${res.tracks[i].name}</span>
+    //     <audio src="${res.tracks[i].previewURL}"controls></audio>   
+    //     </div>
+    //     `
+    //     }
+    //     console.log(res);
+    // })
+      
+    
+const jtest=document.getElementById('test');
+
+
+//fetch top songs
+//fetch top songs of the day
+async function TopOfTheDay(){
+    jtopText.innerHTML='Top Of The Day';
+    juli.classList.add("hiden");
+    ishidden=true;
+    
+    jaudio.innerHTML='';
+    await fetch(`https://api.napster.com/v2.2/tracks/top?range=day`,{
+        
+        method:'GET',
+        headers:{
+            'apikey':'N2EwNWJiZWUtOGVkMi00YmJjLTgwOWMtY2FiMWQ3MDI1ZjYx'
+        }
+    }).then(res=>res.json())
+    .then(function aa(res){
+
+        for(var i=0;i<res.tracks.length;i++){
+        jaudio.innerHTML+=`
+        <div class="box">
+        <img src="https://api.napster.com/imageserver/v2/artists/${res.tracks[i].artistId}/images/356x237.jpg">
+        <span class="subtitle">${res.tracks[i].artistName}</span>
+        <span class="title">${res.tracks[i].name}</span>
+        <audio src="${res.tracks[i].previewURL}"controls></audio>   
+        </div>
+        `
+        }
+        console.log(res);
+    });
+  
+};
+//fetch top songs of the Week
+async function TopOfTheWeek(){
+    jtopText.innerHTML='Top Of The Week';
+    juli.classList.add("hiden");
+    ishidden=true;
+
+    jaudio.innerHTML='';
+    await fetch(`https://api.napster.com/v2.2/tracks/top?range=week`,{
+        
+        method:'GET',
+        headers:{
+            'apikey':'N2EwNWJiZWUtOGVkMi00YmJjLTgwOWMtY2FiMWQ3MDI1ZjYx'
+        }
+    }).then(res=>res.json())
+    .then(function aa(res){
+
+        for(var i=0;i<res.tracks.length;i++){
+        jaudio.innerHTML+=`
+        <div class="box">
+        <img src="https://api.napster.com/imageserver/v2/artists/${res.tracks[i].artistId}/images/356x237.jpg">
+        <span class="subtitle">${res.tracks[i].artistName}</span>
+        <span class="title">${res.tracks[i].name}</span>
+        <audio src="${res.tracks[i].previewURL}"controls></audio>   
+        </div>
+        `
+        }
+        console.log(res);
+    });
+  
+};
+
+//fetch top songs of the Month
+async function TopOfTheMonth(){
+    jtopText.innerHTML='Top Of The Month';
+    juli.classList.add("hiden");
+    ishidden=true;
+
+    jaudio.innerHTML='';
+    await fetch(`https://api.napster.com/v2.2/tracks/top?range=month`,{
+        
+        method:'GET',
+        headers:{
+            'apikey':'N2EwNWJiZWUtOGVkMi00YmJjLTgwOWMtY2FiMWQ3MDI1ZjYx'
+        }
+    }).then(res=>res.json())
+    .then(function aa(res){
+
+        for(var i=0;i<res.tracks.length;i++){
+        jaudio.innerHTML+=`
+        <div class="box">
+        <img src="https://api.napster.com/imageserver/v2/artists/${res.tracks[i].artistId}/images/356x237.jpg">
+        <span class="subtitle">${res.tracks[i].artistName}</span>
+        <span class="title">${res.tracks[i].name}</span>
+        <audio src="${res.tracks[i].previewURL}"controls></audio>   
+        </div>
+        `
+        }
+        console.log(res);
+    });
+  
+};
+
+//fetch top songs of the Year
+async function TopOfTheYear(){
+    jtopText.innerHTML='Top Of The Year';
+    juli.classList.add("hiden");
+    ishidden=true;
+
+    jaudio.innerHTML='';
+    await fetch(`https://api.napster.com/v2.2/tracks/top?range=year`,{
+        
+        method:'GET',
+        headers:{
+            'apikey':'N2EwNWJiZWUtOGVkMi00YmJjLTgwOWMtY2FiMWQ3MDI1ZjYx'
+        }
+    }).then(res=>res.json())
+    .then(function aa(res){
+
+        for(var i=0;i<res.tracks.length;i++){
+        jaudio.innerHTML+=`
+        <div class="box">
+        <img src="https://api.napster.com/imageserver/v2/artists/${res.tracks[i].artistId}/images/356x237.jpg">
+        <span class="subtitle">${res.tracks[i].artistName}</span>
+        <span class="title">${res.tracks[i].name}</span>
+        <audio src="${res.tracks[i].previewURL}"controls></audio>   
+        </div>
+        `
+        }
+      //  console.log(res);
+    });
+  
+};
+
+
+
+
+
+//fetch search result and deploy ===================================
+jenterBTN.addEventListener('click',async function(){
+    jaudio.innerHTML='';
+    jtopText.innerHTML=jinputValue.value;
+
+    await fetch(`https://api.napster.com/v2.2/search?query=${jinputValue.value}&per_type_limit=5`,{
+        
+        method:'GET',
+        headers:{
+            'apikey':'N2EwNWJiZWUtOGVkMi00YmJjLTgwOWMtY2FiMWQ3MDI1ZjYx'
+        }
+    }).then(res=>res.json())
+    .then(function zz(res){
+
+        for(var i=0;i<res.search.data.tracks.length;i++){
+        jaudio.innerHTML+=`
+        <div class="box">
+        <img src="https://api.napster.com/imageserver/v2/artists/${res.search.data.tracks[i].artistId}/images/356x237.jpg">
+        <span class="subtitle">${res.search.data.tracks[i].artistName}</span>
+        <span class="title">${res.search.data.tracks[i].name}</span>
+        <audio src="${res.search.data.tracks[i].previewURL}"controls></audio>   
+        </div>
+        `
+        }
+        //console.log(res.search.data);
+    })
+    
 });
-
-
-
-
-
-
-
-
-
-
-
